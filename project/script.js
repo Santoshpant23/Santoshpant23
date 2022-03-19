09321b12d8158613762934daf249d897ced1cc4a
Santosh pant <72144199+Santoshpant23@users.noreply.github.com>
2022-03-19 14:48:50 +0545

    Add files via upload

---
+++ project/script.js
@@ -0,0 +1,37 @@
+
+let body = document.querySelector("body");
+async function validate() {
+    let sno = document.myform.symbol.value;
+    let date = document.myform.dob.value;
+    if (sno == "" || date == "") {
+        alert("Please enter all details mentioned");
+    }
+    else {
+
+        console.log("Change");
+
+
+
+
+        body.innerHTML = `
+            <div class="img">
+            <img class="imgg"  src="results.png" alt=""   />
+            </div>
+            `;
+
+    }
+
+}
+
+async function change() {
+
+    await document.querySelector(".submit").addEventListener("click", function () {
+
+
+        body.innerHTML = `
+        <div class="img">
+        <img class="imgg"  src="results.png" alt=""   />
+        </div>
+        `;
+    });
+}
